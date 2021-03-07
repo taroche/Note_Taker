32d576409db8af256ec458ba1f47db61b923c62d
Tristan LaRoche <laroche11235@gmail.com>
2021-03-07 13:45:15 -0500

    required fs, path, and db.json

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const db = require("./db/db.json");
+const fs = require("fs");
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 
