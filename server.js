5435b66cc0ee16976af8607e7335e9cbf737e6d9
Tristan LaRoche <laroche11235@gmail.com>
2021-03-07 13:48:06 -0500

    api route for get

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use(express.json());
 
 app.use(express.static(__dirname + '/public'));
 
+//API routes
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        return res.json(JSON.parse(data))
+    })
+
+})
+
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
