69336315eacfe6f99e8126c4911e82862b966da9
Tristan LaRoche <laroche11235@gmail.com>
2021-03-08 19:21:14 -0500

    update what port is on

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const db = require("./db/db.json");
 const fs = require("fs");
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 // parse json request body
